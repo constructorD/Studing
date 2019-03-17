547dc477bcbbbfc0d0a0481403d1189c4777b8ec
constructorD <constructorD@users.noreply.github.com>
2019-03-17 20:25:09 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    static class tempClass implements  tempInterface{
+
+        @Override
+        public void toStr() {
+
+        }
+    }
+    interface tempInterface{
+        void toStr();
+    }
+}
